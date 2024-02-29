35a8a3881cd67979ab8deba587cf4e7f9092b724
Richard Huang <richardhuang.huang@gmail.com>
2024-02-28 22:36:49 -0800

    [main] Add flag to enable profiling

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime/pprof"
 	"sort"
 	"strconv"
 	"strings"
 )
 
 var input = flag.String("input", "", "input file path")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 type stat struct {
 	min   float64
@@ -30,6 +32,17 @@ type stationStats struct {
 
 func main() {
 	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 	result, err := eval(*input)
 	if err != nil {
 		log.Fatal(err)
