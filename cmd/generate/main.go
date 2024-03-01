642174939d208fe99698bb777e151fe87119d240
Richard Huang <richardhuang.huang@gmail.com>
2024-02-29 22:22:43 -0800

    [generate] Add cpu profiler flag

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"runtime/pprof"
 	"time"
 )
 
@@ -438,9 +439,23 @@ var stations = []weatherStation{
 }
 
 var size = flag.Int("size", 0, "number of records to create")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	f, err := os.Create("measurements.txt")
 	if err != nil {
 		log.Fatal("could not create measurements file: ", err)
