75765c7b9f476837fc56869a9d3908beab463b5b
Richard Huang <richardhuang.huang@gmail.com>
2024-03-02 23:38:32 -0800

    [main] Add worker goroutines

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime"
 	"runtime/pprof"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+const batchSize = 10_000_000
 
 var input = flag.String("input", "", "input file path")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
@@ -26,7 +30,7 @@ type stat struct {
 }
 
 type stationStats struct {
-	stats    map[string]*stat
+	stats    map[string]stat
 	stations []string
 }
 
@@ -62,7 +66,7 @@ func eval(fpath string, w io.Writer) error {
 
 // format will take a map of station statistics and a sorted list of stations
 // and return the properly formatted string output
-func format(ss *stationStats, w io.Writer) {
+func format(ss stationStats, w io.Writer) {
 	io.WriteString(w, "{")
 	for i, station := range ss.stations {
 		v := ss.stats[station]
@@ -79,48 +83,111 @@ func format(ss *stationStats, w io.Writer) {
 
 // readStats reads the input file given the file path and returns a map of
 // station statistics and a sorted list of the stations
-func readStats(fpath string) (*stationStats, error) {
-	stations := []string{}
+func readStats(fpath string) (stationStats, error) {
 	file, err := os.Open(fpath)
 	if err != nil {
 		log.Fatal(fmt.Errorf("could not open file: %w", err))
 	}
 	defer file.Close()
 
-	stats := make(map[string]*stat)
+	var wg sync.WaitGroup
+	linesChan := make(chan []string)
+	statsChan := make(chan map[string]stat)
+
+	for i := 0; i < runtime.NumCPU()-1; i++ {
+		wg.Add(1)
+		go worker(&wg, linesChan, statsChan)
+	}
 
 	reader := bufio.NewReader(file)
+	batch := make([]string, 0, batchSize)
 	for {
 		str, err := reader.ReadString('\n')
 		str = strings.TrimSpace(str)
 		if errors.Is(err, io.EOF) {
 			break
 		} else if err != nil {
-			return nil, fmt.Errorf("error reading line: %w", err)
+			return stationStats{},
+				fmt.Errorf("error reading line: %w", err)
 		}
-		station, temp, err := parseLine(str)
-		if err != nil {
-			return nil, fmt.Errorf("line parse error: %w", err)
+		batch = append(batch, str)
+		if len(batch) >= batchSize {
+			linesChan <- batch
+			batch = batch[:0]
 		}
-		if val, ok := stats[station]; ok {
-			val.count += 1
-			val.sum += temp
-			val.min = min(val.min, temp)
-			val.max = max(val.max, temp)
-		} else {
-			stations = append(stations, station)
-			stats[station] = &stat{
-				count: 1,
-				min:   temp,
-				max:   temp,
-				sum:   temp,
+	}
+	if len(batch) > 0 {
+		linesChan <- batch
+	}
+	close(linesChan)
+
+	result := make(chan stationStats)
+
+	go func() {
+		stations := []string{}
+		stats := make(map[string]stat)
+		for partialStats := range statsChan {
+			for k, v := range partialStats {
+				if val, ok := stats[k]; ok {
+					val.count += v.count
+					val.sum += v.sum
+					val.min = min(val.min, v.min)
+					val.max = max(val.max, v.max)
+					stats[k] = val
+				} else {
+					stats[k] = v
+					stations = append(stations, k)
+				}
+			}
+		}
+
+		sort.Strings(stations)
+
+		result <- stationStats{stats, stations}
+	}()
+	wg.Wait()
+	close(statsChan)
+
+	a := <-result
+	close(result)
+
+	return a, nil
+}
+
+// worker processes strings fed to it by the lines channel input and writes its
+// stats map results into the stats channel
+func worker(
+	wg *sync.WaitGroup,
+	linesChan <-chan []string,
+	statsChan chan<- map[string]stat,
+) error {
+	defer wg.Done()
+	stats := make(map[string]stat)
+	for lines := range linesChan {
+		for _, line := range lines {
+			str := strings.TrimSpace(line)
+			station, temp, err := parseLine(str)
+			if err != nil {
+				return fmt.Errorf("line parse error: %w", err)
+			}
+			if val, ok := stats[station]; ok {
+				val.count += 1
+				val.sum += temp
+				val.min = min(val.min, temp)
+				val.max = max(val.max, temp)
+				stats[station] = val
+			} else {
+				stats[station] = stat{
+					count: 1,
+					min:   temp,
+					max:   temp,
+					sum:   temp,
+				}
 			}
 		}
 	}
-
-	sort.Strings(stations)
-
-	return &stationStats{stats, stations}, nil
+	statsChan <- stats
+	return nil
 }
 
 // parseLine will parse an input string of the format "station;temperature" and
